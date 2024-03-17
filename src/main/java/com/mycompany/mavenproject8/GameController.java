d1270669969db2ef3c16295ac3e4adc4a04dea6d
Julio Cesar Amaya Amaya <juliocesaramaya91@hotmail.com>
2024-03-17 16:42:43 -0500

    new update

--- src/main/java/com/mycompany/mavenproject8/GameController.java
+++ src/main/java/com/mycompany/mavenproject8/GameController.java
@@ -46,19 +46,22 @@ import javafx.util.Duration;
  */
 public class GameController implements Initializable {
 
-    private static Player payer;
-    private static Player playerIni;
-    private static Player ;
+    private static Player player;
+    private static Player playerInivitado;
+    private static Player playerSelec;
 
     private final String defaultResour = getClass().getResource("").getPath();
     private final String carpetaImagenes = defaultResour + "imgGame/images";
     private static List<Image> listaImages = new ArrayList<>();
     private static List<PhongMaterial> listMaterials = new ArrayList<>();
+    private List<Box> allBox = new ArrayList<>();
     private Box boxAux;
     private Box boxAux2;
     private double durationAnimation = 0.2;
     private int boxCouter;
-    private int timer;
+
+    private int timerReset = 10;
+    private int timerCounter;
 
     @FXML
     private Label lbTimer;
@@ -149,35 +152,45 @@ public class GameController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        player = PrimaryController.getPlayerselected();
+        playerInivitado = new Player();
+        playerSelector(player);
         boxCouter = 0;
-        payer = PrimaryController.getPlayerselected();
-        namePlayer.setText(payer.getUsuario());
-        timer = 10;
+
+        timerCounter = timerReset;
+        namePlayer.setText(player.getUsuario());
         updatePuntosPlayer(0);
         updatePuntosinvitado(0);
         cargarImgs();
         cargarMateriales();
         setDefaulMaterial();
+        addBoxToList();
 
     }
 
+    public void addBoxToList() {
+        allBox.add(myBox);
+        allBox.add(myBox1);
+        allBox.add(myBox2);
+        allBox.add(myBox3);
+        allBox.add(myBox4);
+        allBox.add(myBox5);
+        allBox.add(myBox6);
+        allBox.add(myBox7);
+        allBox.add(myBox8);
+        allBox.add(myBox9);
+        allBox.add(myBox10);
+        allBox.add(myBox11);
+        allBox.add(myBox12);
+        allBox.add(myBox13);
+        allBox.add(myBox14);
+        allBox.add(myBox15);
+    }
+
     public void boxDisable(boolean Disable) {
-        myBox.setDisable(Disable);
-        myBox1.setDisable(Disable);
-        myBox2.setDisable(Disable);
-        myBox3.setDisable(Disable);
-        myBox4.setDisable(Disable);
-        myBox5.setDisable(Disable);
-        myBox6.setDisable(Disable);
-        myBox7.setDisable(Disable);
-        myBox8.setDisable(Disable);
-        myBox9.setDisable(Disable);
-        myBox10.setDisable(Disable);
-        myBox11.setDisable(Disable);
-        myBox12.setDisable(Disable);
-        myBox13.setDisable(Disable);
-        myBox14.setDisable(Disable);
-        myBox15.setDisable(Disable);
+        for (Box box : allBox) {
+            box.setDisable(Disable);
+        }
     }
 
     private void comparador() {
@@ -194,10 +207,11 @@ public class GameController implements Initializable {
 
                 if (m1.equals(m2)) {
                     System.out.println("los materiales son iguales");
-                    updatePuntosPlayer(payer.getPuntaje() + 1);
+                    updatePuntos(1);
                     boxAux2.setDisable(true);
                     boxAux.setDisable(true);
 
+                    timerCounter = timerReset;
                     boxAux = null;
                 } else {
 
@@ -217,6 +231,15 @@ public class GameController implements Initializable {
         timer.schedule(task, 1000);
     }
 
+    public void updatePuntos(int puntos) {
+        if (playerSelec == player) {
+            updatePuntosPlayer(player.getPuntaje() + puntos);
+        }
+        if (playerSelec == playerInivitado) {
+            updatePuntosinvitado(playerInivitado.getPuntaje() + puntos);
+        }
+    }
+
     public void rotation1(Box box) {
         boxDisable(true);
         RotateTransition rotate = new RotateTransition();
@@ -231,6 +254,12 @@ public class GameController implements Initializable {
 
         });
         rotate.play();
+    }
+
+    public boolean totalPuntosAlcansados() {
+        int totalPuntos = player.getPuntaje()+playerInivitado.getPuntaje();
+        return totalPuntos>=8?true:false;
+
     }
 
     public void rotation11(Box box) {
@@ -416,23 +445,35 @@ public class GameController implements Initializable {
 
             @Override
             public void handle(ActionEvent event) {
-                timer--;
-                lbTimer.setText(Integer.toString(timer));
+                timerCounter--;
+                lbTimer.setText(Integer.toString(timerCounter));
 
                 if (twoPlayer) {
+                    if (totalPuntosAlcansados()) {
+                        System.out.println("Player: " + playerSelec.getUsuario() + " Es el ganador");
 
+                        cuentaRegresiva.stop();
+                    }
+
+                    if (timerCounter <= 0) {
+                        if (playerSelec == player) {
+                            playerSelector(playerInivitado);
+                        } else {
+                            playerSelector(player);
+                        }
+                        timerCounter = timerReset;
+
+                    }
                 } else {
+                    if (timerCounter <= 0) {
+                        boxDisable(true);
+                        cuentaRegresiva.stop();
 
-                }
+                        System.out.println("Player: " + playerSelec.getUsuario() + " Es el ganador");
 
-                if (timer <= 0) {
+                        System.out.println("se acabo el tiempo");
 
-                    cuentaRegresiva.stop();
-
-                    initialize(null, null);
-
-                    System.out.println("se acabo el tiempo");
-
+                    }
                 }
 
             }
@@ -442,6 +483,25 @@ public class GameController implements Initializable {
         cuentaRegresiva.playFromStart();
 
         selectedPlayers.setVisible(false);
+    }
+
+    public void playerSelector(Player p) {
+        playerSelec = p;
+
+        if (playerSelec == player) {
+
+            nameInvitado.setScaleX(1);
+            nameInvitado.setScaleY(1);
+            namePlayer.setScaleX(2);
+            namePlayer.setScaleY(2);
+        } else {
+            nameInvitado.setScaleX(2);
+            nameInvitado.setScaleY(2);
+
+            namePlayer.setScaleX(1);
+            namePlayer.setScaleY(1);
+        }
+
     }
 
     public boolean confirmación(String mensage) {
@@ -464,14 +524,15 @@ public class GameController implements Initializable {
     }
 
     public void updatePuntosPlayer(int puntos) {
-        payer.setPuntaje(puntos);
+        player.setPuntaje(puntos);
         String str = Integer.toString(puntos);
         Platform.runLater(() -> puntosPlayer.setText(str));
     }
 
     public void updatePuntosinvitado(int puntosInvitado) {
 
-        this.puntosInvitado.setText(Integer.toString(puntosInvitado));
+        playerInivitado.setPuntaje(puntosInvitado);
+        Platform.runLater(() -> this.puntosInvitado.setText(Integer.toString(puntosInvitado)));
     }
 
     @FXML
